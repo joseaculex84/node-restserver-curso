5880083f072e0916a8a6ef9b616f8c6325586d47
“JoseDavid” <joseaculex84@gmail.com>
2020-06-13 18:24:05 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Yoyi:Lt1aAYmF933a5HFb@cluster0-n3ahu.mongodb.net/cafe';
+    //urlDB = 'mongodb+srv://Yoyi:Lt1aAYmF933a5HFb@cluster0-n3ahu.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
